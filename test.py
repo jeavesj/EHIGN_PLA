70c93e812e75bb5344570fcd6ba06fc4d4162b7d
Joelle Eaves <jeaves@msu.edu>
2025-11-21 10:04:19 -0500

    Added argparse and device detection (cuda vs cpu)

--- test.py
+++ test.py
@@ -19,6 +19,7 @@ from scipy.stats import pearsonr
 import warnings
 warnings.filterwarnings('ignore')
 
+import argparse
 
 # %%
 def val(model, dataloader, device):
@@ -46,39 +47,30 @@ def val(model, dataloader, device):
     return rmse, pr
 
 if __name__ == '__main__':
-    data_root = './data'
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_dir', type=str, default='./data/toy_set')
+    parser.add_argument('--data_csv', type=str, default='./data/toy_examples.csv')
 
-    valid_dir = os.path.join(data_root, 'valid')
-    test2013_dir = os.path.join(data_root, 'test2013')
-    test2016_dir = os.path.join(data_root, 'test2016')
-    test2019_dir = os.path.join(data_root, 'test2019')
+    args = parser.parse_args()
+    data_dir = args.data_dir
+    data_csv = args.data_csv
+    data_df = pd.read_csv(data_csv)
 
-    train_df = pd.read_csv(os.path.join(data_root, 'train.csv'))
-    valid_df = pd.read_csv(os.path.join(data_root, 'valid.csv'))
-    test2013_df = pd.read_csv(os.path.join(data_root, 'test2013.csv'))
-    test2016_df = pd.read_csv(os.path.join(data_root, 'test2016.csv'))
-    test2019_df = pd.read_csv(os.path.join(data_root, 'test2019.csv'))
+    data_set = GraphDataset(data_dir, data_df, graph_type='Graph_EHIGN', create=False)
 
-    valid_set = GraphDataset(valid_dir, valid_df, graph_type='Graph_EHIGN', create=False)
-    test2013_set = GraphDataset(test2013_dir, test2013_df, graph_type='Graph_EHIGN', create=False)
-    test2016_set = GraphDataset(test2016_dir, test2016_df, graph_type='Graph_EHIGN', create=False)
-    test2019_set = GraphDataset(test2019_dir, test2019_df, graph_type='Graph_EHIGN', create=False)
+    data_loader = DataLoader(data_set, batch_size=128, shuffle=False, collate_fn=collate_fn, num_workers=8)
 
-    valid_loader = DataLoader(valid_set, batch_size=128, shuffle=False, collate_fn=collate_fn, num_workers=8)
-    test2013_loader = DataLoader(test2013_set, batch_size=128, shuffle=False, collate_fn=collate_fn, num_workers=8)
-    test2016_loader = DataLoader(test2016_set, batch_size=128, shuffle=False, collate_fn=collate_fn, num_workers=8)
-    test2019_loader = DataLoader(test2019_set, batch_size=128, shuffle=False, collate_fn=collate_fn, num_workers=8)
-
-    device = torch.device('cuda:0')
+    if torch.cuda.is_available():
+        device = torch.device('cuda:0')
+    else:
+        device = torch.device('cpu')
     model =  DTIPredictor(node_feat_size=35, edge_feat_size=17, hidden_feat_size=256, layer_num=3).to(device)
     load_model_dict(model, './model/20230120_135757_EHIGN_repeat0/model/epoch-144, train_loss-0.5772, train_rmse-0.7598, valid_rmse-1.1799, valid_pr-0.7718.pt')
 
-    valid_rmse, valid_pr = val(model, valid_loader, device)
-    test2013_rmse, test2013_pr = val(model, test2013_loader, device)
-    test2016_rmse, test2016_pr = val(model, test2016_loader, device)
-    test2019_rmse, test2019_pr = val(model, test2019_loader, device)
-    msg = "valid_rmse-%.4f, valid_pr-%.4f, test2013_rmse-%.4f, test2013_pr-%.4f, test2016_rmse-%.4f, test2016_pr-%.4f, test2019_rmse-%.4f, test2019_pr-%.4f," \
-                % (valid_rmse, valid_pr, test2013_rmse, test2013_pr, test2016_rmse, test2016_pr, test2019_rmse, test2019_pr)
+    rmse, pr = val(model, data_loader, device)
+
+    msg = "rmse-%.4f, pr-%.4f," \
+                % (rmse, pr)
     print(msg)
 
 
